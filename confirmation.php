4df264575690622223d5eae2807fa82d30b0dce1
Natas Pijoraitis <nataspijoraitis@gmail.com>
2025-11-27 15:05:31 +0100

    feat: confirmation page gemaakt

---
+++ confirmation.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Confirmation - ClearTorrent</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="checkout-container">
+        <h1>Thank you for your order!</h1>
+        <p>We will send you an email with your order details.</p>
+    </div>
+</body>
+</html>
