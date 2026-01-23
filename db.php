7356466672e7557b7e2479fa44f11657fc439703
Natas Pijoraitis <nataspijoraitis@gmail.com>
2026-01-23 11:38:06 +0100

    fix: database

--- db.php
+++ db.php
@@ -3,6 +3,8 @@ $host = "localhost";
 $db   = "cleartorrent";
 $user = "root";
 $pass = "";
+$user = "cleartorrent_user";
+$pass = "securepass";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
